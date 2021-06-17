4e3bbf3d9aaf5b6c891b1607e3d141ab5a019da1
Willie McCoy III <78430486+wllmccy3@users.noreply.github.com>
2021-06-17 09:45:15 -0700

    need to fix canvas

--- src/space.js
+++ src/space.js
@@ -1,66 +1,19 @@
-const { random } = require("animejs");
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}           
 
-//constants
-const COLOR_SPACE = "black";
-const COLOR_STARS = "white";
-const STAR_NUM = 200;
-const STAR_SIZE = 0.005;
-const STAR_SPEED = 0.05;
-
-//canvas & context setup
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.height = document.documentElement.clientHeight;
-canvas.width = document.documentElement.clientWidth;
-document.body.appendChild(canvas);
-
-//stars setup
-var stars = [];
-var starSpeed = STAR_SPEED * canvas.width;
-var xy = starSpeed * randomSign() * Math.random();
-var yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(sv, 2)) * randomSign();
-for (let i = 0; i < STAR_NUM; i++) {
-    stars[i] = {
-        r: Math.random() * STAR_SIZE * canvas.width / 2,
-        x: Math.floor(Math.random() * canvas.width),
-        y: Math.floor(Math.random() * canvas.height),
-        xv: xv,
-        yv: yv
-    }
-}
-
-//animation loop
-var timeDelta, timeLast = 0;
-requestAnimationFrame(loop);
-
-function loop(timeNow){
-    //calculate the time difference
-    timeDelta = timeNow - timeLast;
-    timeLast = timeNow;
-    //space background
-    ctx.fillStyle = COLOR_SPACE;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //draw the stars
-    ctx.fillStyle = COLOR_STARS;
-    for (let i = 0; i < STAR_NUM; i++) {
-        ctx.beginPath();
-        ctx.arc(stars[i].x, stars[i].y, stars[i].r, 0, Math.PI *2);
-        ctx.fill();
-    }
-    //update star's x position
-    stars[i].x += stars[i].xv * timeDelta * 0.001;
-    //update star's y position
-    stars[i].y += stars[i].yv * timeDelta * 0.001;
-    //stars go to other side once off screen
-    if (stars[i].x < 0 - stars[i].r) {
-        stars[i].x = canvas.width + stars[i].r;
-    } else if (stars[i].x > canvas.width + stars[i].r) {
-        stars[i].x = 0 - stars[i].r;
-    }
-    //call the next frame
-    requestAnimationFrame(loop);
-
-    function randomSign(){
-        return Math.random() >= 0.5 ? 1 : -1;
-    }
+var canvas = document.getElementById("starfield"),
+context = canvas.getContext("2d"),
+stars = 500,
+colorrange = [0,60,240];
+for (var i = 0; i < stars; i++) {
+    var x = Math.random() * canvas.offsetWidth;
+    y = Math.random() * canvas.offsetHeight,
+    radius = Math.random() * 1.2,
+    hue = colorrange[getRandom(0,colorrange.length - 1)],
+    sat = getRandom(50,100);
+    context.beginPath();
+    context.arc(x, y, radius, 0, 360);
+    context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
+    context.fill();
 }
